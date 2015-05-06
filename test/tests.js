877faaad6f3d6fbdb7867d1caedba3c6abf9a728
tomin <noking@gmail.com>
2015-05-06 19:21:46 +0800

    simple test

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,8 @@
 QUnit.test( "hello test", function( assert ) {
     assert.ok( 1 == "1", "Passed!" );
 });
+
+QUnit.test( "fn init", function( assert ) {
+    var obj = new Ajax();
+    assert.ok(typeof obj == "function", "Passed!" );
+});
